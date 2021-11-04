c0a7215e25e64c6ce024586564877236ad22c075
music961 <961boss@gmail.com>
2021-11-04 15:45:53 +0900

    Add OnboardingScreen and Alignment, State hoising

--- app/src/main/java/com/example/pratice1/MainActivity.kt
+++ app/src/main/java/com/example/pratice1/MainActivity.kt
@@ -3,23 +3,13 @@ package com.example.pratice1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import com.example.pratice1.ui.theme.Pratice1Theme
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +31,7 @@ fun Greeting(name: String) {
 
     Surface(color = MaterialTheme.colors.primary,
             modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Row(modifier = Modifier.padding(36.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
                 .padding(bottom = extraPadding)
@@ -58,11 +48,52 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column {
+fun Greetings(names: List<String> = List(1000) { "$it"}) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
-            Greeting(name)
+            Greeting(name = name)
         }
+    }
+}
+
+@Composable
+fun MyApp() {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    }
+    else {
+        Greetings()
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement =  Arrangement.Center,
+            horizontalAlignment =  Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Kotlin!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    Pratice1Theme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
@@ -70,6 +101,7 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun DefaultPreview() {
     Pratice1Theme {
-        MyApp()
+        Greetings()
     }
 }
+
