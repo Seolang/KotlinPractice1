2795297b54207524f4884dd034207dfb1f958d5f
Seolang <dntmdqls0912@gmail.com>
2021-11-04 16:28:03 +0900

    Added animation when expend padding after click "Show more" buttons

--- app/src/main/java/com/example/pratice1/MainActivity.kt
+++ app/src/main/java/com/example/pratice1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pratice1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -29,11 +30,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false)}
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-    Surface(color = MaterialTheme.colors.primary,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+    var expanded by remember { mutableStateOf(false)}
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp
+    )
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
@@ -42,9 +46,10 @@ fun Greeting(name: String) {
                 Text(text = "Hell,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }
+            OutlinedButton(
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
