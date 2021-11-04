8b642cd3e31b84548c5dd5794b59e090f9df8e0e
Seolang <dntmdqls0912@gmail.com>
2021-11-04 16:43:09 +0900

    Extend animation with Spring

--- app/src/main/java/com/example/pratice1/MainActivity.kt
+++ app/src/main/java/com/example/pratice1/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.pratice1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -32,7 +34,11 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     var expanded by remember { mutableStateOf(false)}
     val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
     )
     Surface(
         color = MaterialTheme.colors.primary,
@@ -41,7 +47,7 @@ fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ){
                 Text(text = "Hell,")
                 Text(text = name)
